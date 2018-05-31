fe33978d16ce99eb8725c407e7a71824fe1367d2
yuan <yyhmmwan@163.com>
2018-05-31 19:43:21 +0800

    add other

--- Day07/dict.py
+++ Day07/dict.py
@@ -5,3 +5,34 @@
 # @File      : list.py
 # @Time      : 2018/5/30 17:21
 # @software  : PyCharm
+
+
+def main():
+    scores = {'wj': 99, 'lily': 100, 'yr': 59}
+
+    print(scores['wj'])
+    print(scores['lily'])
+
+    for elem in scores:
+        print('%s\t -->\t%d' % (elem, scores[elem]))
+
+    scores['yr']  = 65
+    scores['zgwl'] = 71
+    scores.update(lm=67, fqh=85)
+    print(scores)
+
+    if 'wzt' in scores:
+        print(scores['wzt'])
+    print(scores.get('wzt'))
+    print(scores.get('wzt',60))
+
+    print(scores.popitem())
+    print(scores.popitem())
+    print(scores.pop('yr'),65)
+
+    scores.clear()
+    print(scores)
+
+
+if __name__ == '__main__':
+    main()
--- Day07/paomadeng.py
+++ Day07/paomadeng.py
@@ -12,12 +12,11 @@ import time
 
 
 def main():
-    content = '北京欢迎你为你开天辟地…………'
-
+    content = '许多年前 你曾是个朴素的少年爱上一个人 就不怕付出自己一生相信爱会永恒 相信每个陌生人相信你会成为最想成为的人习惯说谎 就是变得成熟了吗有一套房子之后 才能去爱别人吗总是以为 成功之后 就能抚平伤痕欲望边埋着 错过的人当青春耗尽 只剩面目可憎'
     while True:
         # 清理屏幕输出
 
-        os.system('clear')
+        os.system('cls')
         print(content)
 
         time.sleep(0.2)
